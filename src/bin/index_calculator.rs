7f0120f1381e793ee55b7a421546c6a8198d9b50
Lccc <2201008916@qq.com>
2026-01-03 22:05:21 +0800

    1.3win

--- src/bin/index_calculator.rs
+++ src/bin/index_calculator.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let basis: i16 = 13;
-    let lar_num: i128 = 832645832946582346583384562;
+    let basis: i16 = 9;
+    let lar_num: i128 = 9832645832946582346583384562;
 
     let index: i128 = lar_num.ilog(basis as i128) as i128;
     println!(
-        "The large number lies in {basis}^{index} and {basis}^{}",
+        "Your large number lies in {basis}^{index} and {basis}^{}",
         index + 1
     )
 }
--- src/bin/loop.rs
+++ src/bin/loop.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut count = 0;
 
     while count < 5 {
-        println!("bithnch");
+        println!("this is a 5-times loop.");
         count += 1;
     }
 }
