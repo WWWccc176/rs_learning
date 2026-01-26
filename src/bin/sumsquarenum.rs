8b81324ec1c44ea530a868d93d6ca46cdb3f70ff
Lccc <2201008916@qq.com>
2026-01-26 22:29:11 +0800

    1.26win

---
+++ src/bin/sumsquarenum.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let ans = sum_oddsquare_num(948000);
+    println!("{ans}");
+}
+
+fn sum_oddsquare_num(x: i128) -> i128 {
+    (1..x).map(|i| i * i).filter(|i| i % 2 != 0).sum() //格式一定要详记！
+}
