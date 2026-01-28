2062474f29756b345e2eed6c22c98baa6c6d1a79
lccc372 <lccc372@gmail.com>
2026-01-28 13:47:16 +0800

    1.28man

--- src/bin/ssdiff.rs
+++ src/bin/ssdiff.rs
@@ -5,6 +5,7 @@ fn main() {
 }
 
 fn ssd(x: u64) -> u64 {
+    //其实可以直接先推出来通项公式，再编程
     let ssqu: u64 = (1..=x).map(|i| i * i).sum();
     let squs: u64 = (1..=x).sum();
     let diff: u64 = squs * squs - ssqu;
