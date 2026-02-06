be1b3e7d38933bf9040600733c840f9f29b34dba
Lccc <2201008916@qq.com>
2026-02-06 17:57:40 +0800

    2.61win

--- src/bin/floatpresision.rs
+++ src/bin/floatpresision.rs
@@ -1,9 +1,15 @@
 fn main() {
     let a: f32 = 0.1;
     let b: f64 = 0.1;
+    let c: f32 = 1.;
+    let d: f64 = 1.;
     let aa: f32 = a + a + a + a + a + a + a + a + a + a;
     let bb: f64 = b + b + b + b + b + b + b + b + b + b;
+    let cc: f32 = c + c + c + c + c + c + c + c + c + c;
+    let dd: f64 = d + d + d + d + d + d + d + d + d + d;
 
-    println!("{:.16}", aa);
-    println!("{:.16}", bb);
+    println!("{:.20}", aa);
+    println!("{:.20}", bb);
+    println!("{:.20}", cc);
+    println!("{:.20}", dd);
 }
